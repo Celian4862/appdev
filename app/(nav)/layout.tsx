24098f0dc21004f57add36fa7f5d5b5b4e093558
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-06-25 16:21:51 +0800

    Created route groups
    Grouped currently existing routes into the nav route group.
    
    Created the nonav group for a later page.

---
+++ app/(nav)/layout.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <nav className="m-7 flex flex-wrap justify-around *:font-bold">
+        <Link href="/" className="flex items-center gap-2">
+          <Image
+            src="/favicon.ico"
+            alt="DevMate Logo"
+            width={25}
+            height={25}
+            className="rounded-full"
+          />
+          <span className="text-3xl">DevMate</span>
+        </Link>
+        <div className="flex flex-wrap items-center gap-10">
+          <Link href="/" className="capitalize">
+            home
+          </Link>
+          <Link href="/features" className="capitalize">
+            features
+          </Link>
+          <Link href="/how-it-works" className="capitalize">
+            how it works
+          </Link>
+          <Link href="/tracks" className="capitalize">
+            tracks
+          </Link>
+          <Link href="/faq" className="uppercase">
+            faq
+          </Link>
+        </div>
+        <Link href="/login" className="block rounded-md border-2 px-5 py-1">
+          Login
+        </Link>
+      </nav>
+      {children}
+    </>
+  );
+}
