cfb44434ae38f7d8d71542ebb75aa0d193efbc10
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-07-09 00:07:43 +0800

    Updated ui/Nav.tsx, added ui/client/NavLink.tsx
    Made changes to some classes.
    
    Moved the link items to the new Client Component ui/client/NavLink.tsx, keeping ui/Nav.tsx as a Server Component.
    
    Also modified the mapping logic in ui/client/NavLink.tsx.

--- ui/Nav.tsx
+++ ui/Nav.tsx
@@ -1,59 +1,15 @@
-"use client";
 import Link from "next/link";
 import Image from "next/image";
 import { FaUserCircle } from "react-icons/fa";
-import { usePathname } from "next/navigation";
+import NavLink from "./client/NavLink";
 
 export default function Nav() {
   const logged_in = true; // Placeholder session variable
-  const pathname = usePathname();
-  const guest = [
-    {
-      route: "/features",
-      name: "features",
-    },
-    {
-      route: "/how-it-works",
-      name: "how it works",
-    },
-    {
-      route: "/tracks",
-      name: "tracks",
-    },
-    {
-      route: "/faq",
-      name: "FAQ",
-    },
-  ];
-  const member = [
-    {
-      route: "/",
-      name: "home",
-    },
-    {
-      route: "/dashboard",
-      name: "dashboard",
-    },
-    {
-      route: "/roadmap",
-      name: "roadmap",
-    },
-    {
-      route: "/assessments",
-      name: "assessments",
-    },
-    {
-      route: "/playground",
-      name: "playground",
-    },
-  ];
   return (
     <nav
       className={`${
-        logged_in
-          ? "fixed top-0 left-0 z-50 w-full border-b-2 border-white bg-black py-8"
-          : "m-7"
-      } flex flex-wrap items-center justify-around *:font-bold`}
+        logged_in && "border-b-2 border-white bg-black"
+      } fixed z-50 flex w-full flex-wrap items-center justify-around py-8 *:font-bold`}
     >
       <Link href="/" className="flex items-center gap-2">
         <Image
@@ -65,32 +21,8 @@ export default function Nav() {
         />
         <span className="text-3xl">DevMate</span>
       </Link>
-      <div className="flex flex-wrap items-center gap-10 *:capitalize">
-        {logged_in ? (
-          <>
-            {member.map((links, index) => (
-              <Link
-                key={index}
-                href={links.route}
-                className={`rounded-md px-3 py-1 transition-colors duration-300 ${
-                  pathname === links.route
-                    ? "bg-white text-black"
-                    : "hover:bg-white hover:text-black"
-                }`}
-              >
-                {links.name}
-              </Link>
-            ))}
-          </>
-        ) : (
-          <>
-            {guest.map((links, index) => (
-              <Link key={index} href={links.route}>
-                {links.name}
-              </Link>
-            ))}
-          </>
-        )}
+      <div className="flex flex-wrap items-center gap-10">
+        <NavLink logged_in={logged_in} />
       </div>
       {logged_in ? (
         <>
