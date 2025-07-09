cf49a40f931a580cc60215d79b50ea1b3f5b62f9
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-07-09 12:02:09 +0800

    Updated NavLink.tsx
    Moved the array of links to a variable.

--- ui/client/NavLink.tsx
+++ ui/client/NavLink.tsx
@@ -4,24 +4,25 @@ import { usePathname } from "next/navigation";
 
 export default function NavLink({ logged_in }: { logged_in: boolean }) {
   const pathname = usePathname();
+  const links = logged_in
+    ? ["home", "dashboard", "roadmap", "assessments", "playground"]
+    : ["home", "features", "how-it-works", "tracks", "faq"];
 
   return logged_in
-    ? ["home", "dashboard", "roadmap", "assessments", "playground"].map(
-        (link, i) => (
-          <Link
-            key={i}
-            href={link === "home" ? "/" : `/${link}`}
-            className={`rounded-md px-3 py-1 capitalize transition-colors duration-300 ${
-              pathname === `/${link === "home" ? "" : link}`
-                ? "bg-white text-black"
-                : "hover:bg-white hover:text-black"
-            }`}
-          >
-            {link.replace("-", " ")}
-          </Link>
-        ),
-      )
-    : ["home", "features", "how-it-works", "tracks", "faq"].map((link, i) => (
+    ? links.map((link, i) => (
+        <Link
+          key={i}
+          href={link === "home" ? "/" : `/${link}`}
+          className={`rounded-md px-3 py-1 capitalize transition-colors duration-300 ${
+            pathname === `/${link === "home" ? "" : link}`
+              ? "bg-white text-black"
+              : "hover:bg-white hover:text-black"
+          }`}
+        >
+          {link.replace("-", " ")}
+        </Link>
+      ))
+    : links.map((link, i) => (
         <Link
           key={i}
           href={link === "home" ? "/" : `/#${link}`}
